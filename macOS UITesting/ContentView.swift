063500e41a72839d1a35bc5b3037fa391c4ced40
Rudrank Riyam <rudrankriyam@gmail.com>
2022-01-12 17:20:21 +0530

    Add speech recognizer

--- macOS UITesting/ContentView.swift
+++ macOS UITesting/ContentView.swift
@@ -6,66 +6,95 @@
 //
 
 import SwiftUI
+import Speech
 
 struct ContentView: View {
-  @State private var showWelcomeLabel = false
+  @StateObject private var viewModel = ContentViewModel()
   
   var body: some View {
     VStack {
-      if showWelcomeLabel {
+      if viewModel.showWelcomeLabel {
         Text("Welcome to Codemagic!")
           .font(.largeTitle)
           .padding()
       }
       
       Button("Show") {
-        takeScreensShots(folderName: "macOS_uitesting")
-        showWelcomeLabel.toggle()
+        viewModel.takeScreensShots(folderName: "macOS_uitesting")
+        viewModel.showWelcomeLabel.toggle()
+        
+        viewModel.speech()
       }
       .accessibilityIdentifier("Show Button")
     }
     .frame(width: 500, height: 500, alignment: .center)
   }
   
-  func takeScreensShots(folderName: String){
-      
-      var displayCount: UInt32 = 0;
-      var result = CGGetActiveDisplayList(0, nil, &displayCount)
-      if (result != CGError.success) {
-          print("error: \(result)")
-          return
-      }
-      let allocated = Int(displayCount)
-      let activeDisplays = UnsafeMutablePointer<CGDirectDisplayID>.allocate(capacity: allocated)
-      result = CGGetActiveDisplayList(displayCount, activeDisplays, &displayCount)
-      
-      if (result != CGError.success) {
-          print("error: \(result)")
-          return
-      }
-         
-      for i in 1...displayCount {
-          let unixTimestamp = CreateTimeStamp()
-          let fileUrl = URL(fileURLWithPath: folderName + "\(unixTimestamp)" + "_" + "\(i)" + ".jpg", isDirectory: true)
-          let screenShot:CGImage = CGDisplayCreateImage(activeDisplays[Int(i-1)])!
-          let bitmapRep = NSBitmapImageRep(cgImage: screenShot)
-          let jpegData = bitmapRep.representation(using: NSBitmapImageRep.FileType.jpeg, properties: [:])!
-          
-          do {
-              try jpegData.write(to: fileUrl, options: .atomic)
-          }
-          catch {print("error: \(error)")}
-      }
-  }
-
-  func CreateTimeStamp() -> Int32
-  {
-      return Int32(Date().timeIntervalSince1970)
-  }
+  
 }
+
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
   }
+}
+
+class ContentViewModel: NSObject, ObservableObject {
+  @Published var showWelcomeLabel = false
+
+  let audioEngine = AVAudioEngine()
+  var speechRecognizer = SFSpeechRecognizer()
+  let request = SFSpeechAudioBufferRecognitionRequest()
+  var recognitionTask: SFSpeechRecognitionTask?
+  
+  
+  func takeScreensShots(folderName: String){
+    
+    var displayCount: UInt32 = 0;
+    var result = CGGetActiveDisplayList(0, nil, &displayCount)
+    if (result != CGError.success) {
+      print("error: \(result)")
+      return
+    }
+    let allocated = Int(displayCount)
+    let activeDisplays = UnsafeMutablePointer<CGDirectDisplayID>.allocate(capacity: allocated)
+    result = CGGetActiveDisplayList(displayCount, activeDisplays, &displayCount)
+    
+    if (result != CGError.success) {
+      print("error: \(result)")
+      return
+    }
+    
+    for i in 1...displayCount {
+      let unixTimestamp = CreateTimeStamp()
+      let fileUrl = URL(fileURLWithPath: folderName + "\(unixTimestamp)" + "_" + "\(i)" + ".jpg", isDirectory: true)
+      let screenShot:CGImage = CGDisplayCreateImage(activeDisplays[Int(i-1)])!
+      let bitmapRep = NSBitmapImageRep(cgImage: screenShot)
+      let jpegData = bitmapRep.representation(using: NSBitmapImageRep.FileType.jpeg, properties: [:])!
+      
+      do {
+        try jpegData.write(to: fileUrl, options: .atomic)
+      }
+      catch {print("error: \(error)")}
+    }
+  }
+  
+  func CreateTimeStamp() -> Int32 {
+    return Int32(Date().timeIntervalSince1970)
+  }
+  
+  func speech() {
+    speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))
+    speechRecognizer?.delegate = self
+    
+    SFSpeechRecognizer.requestAuthorization { [self] authStatus in
+      if authStatus == .authorized {
+      }
+    }
+  }
+}
+
+extension ContentViewModel: SFSpeechRecognizerDelegate {
+  
 }
